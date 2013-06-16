705a215cd799c61dfac932fb75cb1ae66608dd08
ashleydavis <ashleydavis75@gmail.com>
2013-06-16 18:15:43 +1000

    First pass: draggable data-bound flow chart nodes.

---
+++ app.js
@@ -0,0 +1,81 @@
+
+//
+// Define the 'app' module.
+//
+angular.module('app', ['flowChart', ])
+
+//
+// Application controller.
+//
+.controller('AppCtrl', function AppCtrl ($scope) {
+
+	//
+	// Setup the data-model for the chart.
+	//
+	var chart = {
+
+		nodes: [
+			{
+				title: "Example Node 1",
+				x: 0,
+				y: 0,
+				inputConnectors: [
+					{
+						name: "A",
+					},
+					{
+						name: "B",
+					},
+					{
+						name: "C",
+					},
+				],
+				outputConnectors: [
+					{
+						name: "A",
+					},
+					{
+						name: "B",
+					},
+					{
+						name: "C",
+					},
+				],
+			},
+
+			{
+				title: "Example Node 2",
+				x: 150,
+				y: 200,
+				inputConnectors: [
+					{
+						name: "A",
+					},
+					{
+						name: "B",
+					},
+					{
+						name: "C",
+					},
+				],
+				outputConnectors: [
+					{
+						name: "A",
+					},
+					{
+						name: "B",
+					},
+					{
+						name: "C",
+					},
+				],
+			},
+		],
+
+	};
+
+	$scope.chart = chart;
+
+})
+
+;
