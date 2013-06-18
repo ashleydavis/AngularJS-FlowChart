7dbdd3fa16b78f8a5ea22c1f367fd1f2d7bd10bd
ashleydavis <ashley@codecapers.com.au>
2013-06-19 07:35:27 +1000

    Can now drag a straight line out from an input connector.

--- flowchart/flowchart_directive.js
+++ flowchart/flowchart_directive.js
@@ -12,6 +12,32 @@ angular.module('flowChart', ['dragging'] )
 
   	link: function($scope, $element, $attrs) {
 
+  		//
+  		// Init data-model variables.
+  		//
+  		$scope.draggingConnection = false;
+
+  		//
+  		// Compute the position of a connector relative to its node.
+  		//
+  		$scope.computeLocalInputConnectorX = function (connectorIndex) {
+			return 80;
+		};
+
+  		$scope.computeLocalInputConnectorY = function (connectorIndex) {
+			return 100 + (connectorIndex * 36.5);
+		};
+
+  		//
+  		// Compute the position of a connector in the graph.
+  		//
+  		var computeConnectorPos = function (node, connectorIndex) {
+  			return {
+  				x: node.x + $scope.computeLocalInputConnectorX(connectorIndex),
+  				y: node.y + $scope.computeLocalInputConnectorY(connectorIndex),
+  			};
+  		};
+
 		$scope.nodeMouseDown = function (evt, nodeIndex) {
 
 			var nodes = $scope.chart.nodes;
@@ -47,6 +73,35 @@ angular.module('flowChart', ['dragging'] )
 
 			});
 
+		};
+
+		$scope.inputConnectorMouseDown = function (evt, node, connector, connectorIndex) {
+
+			dragging.startDrag(evt, {
+
+				dragging: function (deltaX, deltaY, x, y) {
+
+					$scope.dragPoint.x = x;
+					$scope.dragPoint.y = y;
+				},
+
+				dragStarted: function (x, y) {
+					$scope.draggingConnection = true;
+					$scope.draggingConnectorPos = computeConnectorPos(node, connectorIndex);
+					$scope.dragPoint = {
+						x: x,
+						y: y
+					};
+				},
+
+				dragEnded: function () {
+					$scope.draggingConnection = false;
+					delete $scope.draggingConnection;
+					delete $scope.draggingConnectorPos;
+					delete $scope.dragPoint;
+				},
+
+			});
 		};
 
   	},
